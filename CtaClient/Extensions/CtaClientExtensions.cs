fa5c59a8f63e03d312960507c4689e0c677d4e02
Jon Beaulieu <jon@jonbeaulieu.com>
2025-12-19 17:08:34 -0600

    Create client extension method for easy service registration

---
+++ CtaClient/Extensions/CtaClientExtensions.cs
@@ -0,0 +1,30 @@
+using CtaClient.Config;
+using CtaClient.Interfaces;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CtaClient.Extensions;
+
+/// <summary>
+///   Extension methods for CTA Client service registration.
+/// </summary>
+public static class CtaClientExtensions
+{
+    /// <summary>
+    ///   Extension method to register a <see cref="CtaClient" /> and corresponding endpoints specified
+    ///   by <see cref="CtaEndpointFactory" /> in a <see cref="IServiceCollection" />.
+    /// </summary>
+    /// <param name="services"><see cref="IServiceCollection" /> to modify</param>
+    /// <param name="config"><see cref="IConfiguration" /> to register for <see cref="CtaApiSettings" /> options</param>
+    public static IServiceCollection AddCtaClient(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<CtaApiSettings>(configuration.GetSection(nameof(CtaApiSettings)));
+
+        services.AddHttpClient<ICtaClient, CtaClient>()
+            .SetHandlerLifetime(TimeSpan.FromMinutes(3));
+
+        services.AddTransient<CtaEndpointFactory>();
+        services.AddSingleton<ICtaClient, CtaClient>();
+        return services;
+    }
+}
