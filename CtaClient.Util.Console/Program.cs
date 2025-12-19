251d78aaa782b3cfcda857278d6885478ae395b1
Jon Beaulieu <jon@jonbeaulieu.com>
2025-12-19 17:12:46 -0600

    Sample console app

---
+++ CtaClient.Util.Console/Program.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+using Serilog;
+using CtaClient.Extensions;
+using CtaClient.Models;
+
+namespace CtaClient.Util.Console;
+
+public static class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .AddEnvironmentVariables()
+            .Build();
+
+        var services = CreateServices(configuration);
+        CtaClient client = services.GetRequiredService<CtaClient>();
+
+        var jsonOptions = new JsonSerializerOptions { WriteIndented = true };
+
+        var request = new ArrivalsRequest
+        {
+            MapId = 41320,
+            MaxResults = 2
+        };
+
+        var result = await client.GetArrivals(request);
+
+        System.Console.WriteLine(JsonSerializer.Serialize(result, jsonOptions));
+    }
+
+    private static ServiceProvider CreateServices(IConfiguration configuration)
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddCtaClient(configuration)
+            .AddSerilog(new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.Debug()
+                .MinimumLevel.Debug()
+                .CreateLogger())
+            .BuildServiceProvider();
+
+        return serviceProvider;
+    }
+}
